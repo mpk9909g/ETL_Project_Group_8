7e368cd8ebe5f7cc7023ec3601134d9aa62c8484
mpk9909g <mpk9909@gmail.com>
2021-11-16 21:36:39 -0600

    initial draft of notebook and sql from Tue 11.16

--- query.sql
+++ query.sql
@@ -1,10 +1,11 @@
 -- Query to check successful load
-SELECT * FROM premise;
+SELECT * FROM listings;
 
-SELECT * FROM county;
+SELECT * FROM air_quality;
+
 
 -- Join tables on county_id
-SELECT premise.id, premise.premise_name, county.county_name
-FROM premise
-INNER JOIN county
-ON premise.county_id = county.county_id;
+SELECT listings.id, listings.state_initial, listings.total_listing_count, listings.county, air_quality.median_aqi
+FROM listings
+INNER JOIN air_quality
+ON listings.state = air_quality.state and listings.county = air_quality.county;
--- schema.sql
+++ schema.sql
@@ -1,13 +1,22 @@
 -- Create Two Tables
-CREATE TABLE premise (
+CREATE TABLE listings (
   id INT PRIMARY KEY,
-  premise_name TEXT,
-  county_id INT
+  county TEXT,
+  state_initial TEXT,
+  state TEXT,
+  total_listing_count INT,
+  average_price INT,
+  median_listing_price INT,
+  active_listing_count INT,
+  median_days_on_market INT
 );
 
-CREATE TABLE county (
+
+
+CREATE TABLE air_quality (
   id INT PRIMARY KEY,
-  county_name TEXT,
-  license_count INT,
-  county_id INT
+  state TEXT,
+  county TEXT,
+  year INT,
+  median_aqi INT
 );
